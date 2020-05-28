08a390f7379e57156fc60c98ee056241ff75741a
Veto Ramirez <vetorobert3@gmail.com>
2020-05-27 22:29:03 -0700

    Not able to migrate:latest. Working on problom.

--- data/migrations/20200527194701_create-tables.js
+++ data/migrations/20200527194701_create-tables.js
@@ -3,20 +3,41 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('zoos', tbl => {
       tbl.increments('id');
-      tbl.string('zoos_name', 128).notNullable();
+      tbl.string('zoo_name', 128).notNullable();
       tbl.string('address', 128).notNullable().unique();
     })
     .createTable('species', tbl => {
       tbl.increments('id');
-      tbl.sring('species_name', 128).notNullable();
+      tbl.string('species_name', 128).notNullable();
     })
     .createTable('animals', tbl => {
       tbl.increments('id');
       tbl.string('animal_name', 128);
-      tbl.integer('species_id').notNullable().unsigned()
+      tbl.integer('species_id')
+        .notNullable()
+        .unsigned()
+        .references('species.id');
+    })
+    .createTable('zoo_animals', tbl => {
+      tbl.integer('zoo_id')
+          .notNullable()
+          .unsigned()
+          .references('zoos.id')
+      tbl.integer('animal_id')
+          .notNullable()
+          .unsigned()
+          .references('animals.id');
+      // Create a move-to-date field
+      // create a move-from-date field
+      // create an increments()
+      tbl.primary(['zoo_id', 'animal_id']);
     })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExist('zoo-animals')
+    .dropTableIfExist('animals')
+    .dropTableIfExist('species')
+    .dropTableIfExist('zoos');
 };
