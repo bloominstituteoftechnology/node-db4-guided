27e62e15a285dc70c250160720f34fdf179847bc
James Blacklock <jamesblacklock@protonmail.com>
2022-08-18 12:59:36 -0400

    committing to "lecture"

--- data/migrations/20220818165155_create_tables.js
+++ data/migrations/20220818165155_create_tables.js
@@ -1,15 +1,28 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('animals', tbl => {
+            tbl.increments('animal_id');
+            tbl.varchar('animal_name', 100);
+            tbl.integer('species_id')
+                .references('species_id')
+                .inTable('species');
+        })
+        .createTable('zoos', tbl => {
+            tbl.increments('zoo_id');
+            tbl.varchar('zoo_name', 100);
+            tbl.varchar('address', 200);
+        })
+        .createTable('zoo_animals', tbl => {
+            //     zoo_id
+            //     animal_id
+        })
+        .createTable('species', tbl => {
+            tbl.increments('species_id');
+            tbl.varchar('species_name', 100);
+        });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+
 };
