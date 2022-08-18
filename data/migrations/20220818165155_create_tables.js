55e1621f1308add968772150752fd45389b6264c
James Blacklock <jamesblacklock@protonmail.com>
2022-08-18 13:13:54 -0400

    committing to "lecture"

--- data/migrations/20220818165155_create_tables.js
+++ data/migrations/20220818165155_create_tables.js
@@ -3,27 +3,35 @@ exports.up = function (knex) {
     return knex.schema
         .createTable('animals', tbl => {
             tbl.increments('animal_id');
-            tbl.varchar('animal_name', 100);
+            tbl.varchar('animal_name', 100)
+                .notNullable();
             tbl.integer('species_id')
+                .notNullable()
                 .references('species_id')
                 .inTable('species');
         })
         .createTable('zoos', tbl => {
             tbl.increments('zoo_id');
-            tbl.varchar('zoo_name', 100);
-            tbl.varchar('address', 200);
+            tbl.varchar('zoo_name', 100)
+                .notNullable();
+            tbl.varchar('address', 200)
+                .notNullable();
         })
         .createTable('zoo_animals', tbl => {
             tbl.integer('zoo_id')
+                .notNullable()
                 .references('zoo_id')
                 .inTable('zoos');
             tbl.integer('animal_id')
+                .notNullable()
                 .references('animal_id')
                 .inTable('animals');
+            tbl.primary(['zoo_id', 'animal_id']);
         })
         .createTable('species', tbl => {
             tbl.increments('species_id');
-            tbl.varchar('species_name', 100);
+            tbl.varchar('species_name', 100)
+                .notNullable();
         });
 };
 
